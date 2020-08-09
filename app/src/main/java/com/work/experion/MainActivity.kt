600dae688861c7506bc7169650878623fcd6e314
Rahul P Raju <rpr091@gmail.com>
2020-08-09 17:36:42 +0530

    FeatureListFragment.kt added in MainActivity.kt

--- app/src/main/java/com/work/experion/MainActivity.kt
+++ app/src/main/java/com/work/experion/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.work.experion
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.title = ""
         setSupportActionBar(findViewById(R.id.toolbar))
-
+        val ft =
+            supportFragmentManager.beginTransaction()
+        val fragment: Fragment = FeatureListFragment.newInstance()
+        ft.replace(R.id.content_home, fragment)
+        ft.commit()
     }
 }
