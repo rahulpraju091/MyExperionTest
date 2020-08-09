012726c4675e28c798d91a8f17bbe54bb66af231
Rahul P Raju <rpr091@gmail.com>
2020-08-09 19:52:13 +0530

    Comments added. Indentations has set.

--- app/src/main/java/com/work/experion/common/Coroutines.kt
+++ app/src/main/java/com/work/experion/common/Coroutines.kt
@@ -4,8 +4,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+/**
+ * Coroutines class. */
 object Coroutines {
-    fun main(work: suspend (()-> Unit)) =
+    fun main(work: suspend (() -> Unit)) =
         CoroutineScope(Dispatchers.Main).launch {
             work()
         }
