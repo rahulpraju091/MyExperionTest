fd6609403c8bca3f656b1f82737bcea2dd80760a
Rahul P Raju <rpr091@gmail.com>
2020-08-09 18:43:11 +0530

    API integration in FeatureListViewModel.kt and FeatureListRepository.kt

--- app/src/main/java/com/work/experion/FeatureListRepository.kt
+++ app/src/main/java/com/work/experion/FeatureListRepository.kt
@@ -2,12 +2,16 @@ package com.work.experion
 
 import com.work.experion.interfaces.AppInterfaceAPI
 import com.work.experion.model.CityModel
+import com.work.experion.model.FeatureModel
 import retrofit2.Response
 
-class FeatureListRepository(private val api: AppInterfaceAPI) {
-
+class FeatureListRepository(
+    private val api: AppInterfaceAPI,
+    private val featureDAO: FeatureDAO
+) {
     suspend fun doGetLocationDetails(): Response<CityModel> {
         return api.doGetLocationDetails()
     }
+
 
 }
--- app/src/main/java/com/work/experion/viewModel/FeatureListViewModel.kt
+++ app/src/main/java/com/work/experion/viewModel/FeatureListViewModel.kt
@@ -1,12 +1,42 @@
 package com.work.experion.viewModel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.work.experion.FeatureListRepository
+import com.work.experion.common.Coroutines
+import com.work.experion.interfaces.APICallbackListener
+import com.work.experion.model.FeatureModel
 
 class FeatureListViewModel(private var featureListRepository: FeatureListRepository) : ViewModel() {
 
     companion object {
         private val TAG: String = FeatureListViewModel::class.java.simpleName.toString()
     }
+
+    private var _mutableTitle = MutableLiveData<String?>()
+    val title: LiveData<String?>?
+        get() = _mutableTitle
+    private var _mutableFeaturesList = MutableLiveData<ArrayList<FeatureModel?>>()
+    val featuresList: LiveData<ArrayList<FeatureModel?>>?
+        get() = _mutableFeaturesList
+
+    fun doGetLocationDetails(listener: APICallbackListener) {
+        Coroutines.main {
+            val response = featureListRepository.doGetLocationDetails()
+            Log.d(TAG, "Response:::" + response.body().toString())
+            if (response.body()?.rows.isNullOrEmpty()) {
+                listener.onResponseFailure()
+            } else if (response.isSuccessful && response.body()?.rows!!.isNotEmpty()) {
+                _mutableTitle.value = response.body()!!.title
+                _mutableFeaturesList.value = response.body()!!.rows
+                listener.onResponseSuccess(response.body()!!)
+            } else {
+                listener.onResponseNull()
+            }
+        }
+    }
+
 
 }
